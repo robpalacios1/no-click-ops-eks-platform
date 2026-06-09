8dd7e7dc55f07134e18ad1ac0634c0f5784407ad
roberto <robpalacios1@hotmail.com>
2026-06-09 11:33:07 -0500

    create Outputs.tf for VPC module

---
+++ terraform/modules/vpc/outputs.tf
@@ -0,0 +1,25 @@
+output "vpc_id" {
+    value = aws_vpc.main_vpc.id
+}
+
+output "public_subnets_ids"{
+    value = [
+        aws_subnet.public_subnet_1.id,
+        aws_subnet.public_subnet_2.id
+    ]
+}
+
+output "private_subnets_ids"{
+    value = [
+        aws_subnet.private_subnet_1.id,
+        aws_subnet.private_subnet_2.id
+    ]
+}
+
+output "public_security_group_id" {
+    value = aws_security_group.public_security_group.id
+}
+
+output "private_security_group_id" {
+    value = aws_security_group.private_security_group.id
+}
